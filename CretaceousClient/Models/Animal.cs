19476cff17d0bae27199f32d22ed17fc6373679a
David Jandron <jandron1211@gmail.com>
2023-03-29 05:33:27 -0700

    ADD views pages and controller items to interact with info on api

--- CretaceousClient/Models/Animal.cs
+++ CretaceousClient/Models/Animal.cs
@@ -22,5 +22,33 @@ namespace CretaceousClient.Models
 
       return animalList;
     }
+
+    public static Animal GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Animal animal = JsonConvert.DeserializeObject<Animal>(jsonResponse.ToString());
+
+      return animal;
+    }
+
+    public static void Post(Animal animal)
+    {
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      ApiHelper.Post(jsonAnimal);
+    }
+
+    public static void Put(Animal animal)
+    {
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      ApiHelper.Put(animal.AnimalId, jsonAnimal);
+    }
+
+    public static void Delete(int id)
+    {
+      ApiHelper.Delete(id);
+    }
   }
 }
